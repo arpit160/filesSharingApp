e88bc13e73a95b189805b9c4f3176642203e1237
arpit160 <singhalarpit160@gmail.com>
2021-02-22 14:49:53 +0530

    changed port to PORT

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.post('/send/:uuid',async(req,res,next)=>
   return res.redirect("/file/upload")
 })
 
-let port=process.env.port || 3000
+let port=process.env.PORT || 3000
 
 app.listen(port,()=>
 {
